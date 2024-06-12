96f9015a016077aebbdb4b479a1aab7136c05f71
Mr-Beastman <Benjamin.d.eastman@gmail.com>
2024-06-13 10:39:58 +1200

    Add sperate files for main and function Seperating main and functions for ease of reading.

---
+++ CS103_A2/functions.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+#include "functions.h"
+
+using namespace std;
+
+//test for calling functions from seperate file.
+void test() {
+    cout << "This is now being called from a seperate file.\n";
+}
--- CS103_A2/main.cpp
+++ CS103_A2/main.cpp
@@ -1,7 +1,14 @@
+//included libraries
 #include<iostream>
+
+//header file that allows us to call functions from a seperate file.
+#include "functions.h"
 
 using namespace std;
 
 int main() {
     //Welcome to out collab file.
+
+    //below is not declaed in main but instead functions.cpp
+    test();
 }
